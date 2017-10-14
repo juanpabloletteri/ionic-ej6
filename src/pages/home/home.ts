87d8c41ad40a9627935a7f8549389e0553b17436
alumno <alumno>
2017-10-14 10:22:41 -0300

    menu OK

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,9 +10,14 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class HomePage {
 
-  usuario: string;
-  email: string;
+  usuario: string = "";
+  email: string = "";
   pass: string;
+
+  pass1: string = "";
+  pass2: string = "";
+
+  esUsuario: boolean = true;
 
   usuarios: FirebaseListObservable<any>;
 
@@ -21,15 +26,10 @@ export class HomePage {
     console.log(this.usuarios);
   }
   login() {
+
     this.usuarios.forEach(element => {
-      for (var i = 0; i < 5; i++) {
+      for (let i in element) {
         if (element[i].nombre == this.usuario && element[i].clave == this.pass) {
-          //SE ENCONTRO USUARIO
-          let alert = this.alertCtrl.create({
-            title: 'Bienvenido: ' + this.usuario,
-            buttons: ['OK']
-          });
-          alert.present();
           //Redirijo a la pagina correspondiente
           this.navCtrl.push(AplicacionPage, { "usuario": this.usuario, "email": this.email });
           return;
@@ -43,27 +43,83 @@ export class HomePage {
       });
       alert.present();
     })
-    /*VERIFICACION FICTICIA
-    if (this.usuario == "admin" && this.pass == "admin") {
-      let alert = this.alertCtrl.create({
-        title: 'Usuario valido!' + this.usuario,
-        subTitle: 'Bienvenido a la aplicacion!',
-        buttons: ['OK']
-      });
-      alert.present();
-      this.navCtrl.push(AplicacionPage, { "usuario": this.usuario, "pass": this.pass });
+  }
 
+  registrar() {
+    console.log(this.usuario, this.email, this.pass1, this.pass2);
+
+    this.usuarios.forEach(element => {
+      for (var i = 0; i < 5; i++) {
+        if (element[i].nombre == this.usuario) {
+          //USUARIO REPETIDO
+          let alert = this.alertCtrl.create({
+            title: 'Usuario Repetido',
+            subTitle: 'Por favor seleccione otro nombre de usuario para ingresar al sistema',
+            buttons: ['OK']
+          });
+          alert.present();
+          this.usuario = "";
+          return;
+        }
+      }
+      //CONTRASEÑAS NO IDENTICAS
+      if (this.pass1 != this.pass2) {
+        let alert = this.alertCtrl.create({
+          title: 'Error en la contraseña',
+          subTitle: 'Las contraseñas ingresadas no son iguales, por favor vuelva a ingresarlas',
+          buttons: ['OK']
+        });
+        alert.present();
+        this.pass1 = "";
+        this.pass2 = "";
+        return;
+      }
+      //CAMPOS VACIOS
+      if (this.usuario == "" || this.email == "" || this.pass1 == "" || this.pass2 == "") {
+        let alert = this.alertCtrl.create({
+          title: 'Campos vacios',
+          subTitle: 'Por favor complete la totalidad de los campos a fin de poder registrarse',
+          buttons: ['OK']
+        });
+        alert.present();
+        return;
+      }
+      //VERIFICACIONES CUMPLIDAS
+      this.usuarios.push({
+        $key: this.usuario,
+        clave: this.pass1,
+      })
+
+    })
+  }
+
+  asignarUsuario(tipo) {
+    switch (tipo) {
+      case "admin": {
+        this.usuario = "admin";
+        this.pass = "11";
+        break;
+      }
+      case "invitado": {
+        this.usuario = "invitado";
+        this.pass = "22";
+        break;
+      }
+      case "usuario": {
+        this.usuario = "usuario";
+        this.pass = "33";
+        break;
+      }
+      case "j1": {
+        this.usuario = "j1";
+        this.pass = "44";
+        break;
+      }
+      case "j2": {
+        this.usuario = "j2";
+        this.pass = "55";
+        break;
+      }
     }
-    else {
-      let alert = this.alertCtrl.create({
-        title: 'Usuario no valido!',
-        subTitle: 'Por favor registrese',
-        buttons: ['NO']
-      });
-      alert.present();
-      this.usuario = "";
-      this.email = "";
-      this.pass = "";
-    }*/
   }
 }
